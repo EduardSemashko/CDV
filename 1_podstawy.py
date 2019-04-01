3112e4333885cf6c6d800bccda0bca1bff5c1b57
EduardSemashko <edwardsemashko0197@gmail.com>
2019-04-01 12:46:33 +0200

    123

---
+++ 1_podstawy.py
@@ -0,0 +1,29 @@
+print("CDV")
+print(2)
+
+potega=2**10
+print(potega)
+
+#pobieranie z klawiatury
+imie=input()
+print("Twoje imie"+imie)
+nazwisko =input ("Podaj nazwisko")
+print("Twoje nazw "+nazwisko)
+print("Towje nazw"+nazwisko ,"Twoje imie"+ imie)
+print ("Podaj swoj wiek:",end="")
+wiek = input()
+print("Twoj wiek",wiek,"lat")
+nazwisko ="Kowalski"
+pierwszyZnak=nazwisko[0]
+print(pierwszyZnak)
+
+ostatniZnak= nazwisko[len(nazwisko)-1]
+print(ostatniZnak)
+
+ostatniZnak=nazwisko[-1]
+print(ostatniZnak)
+#konwersja
+x="5"
+print(type(x))
+x=int(x)
+print(type(x))
